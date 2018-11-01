9a60e16d44a27121485072635b5b489404fc068e
Taufeq Mohammed <taufeq_star@Taufeqs-iMac.local>
2018-11-01 21:47:51 +0530

    Much better now

--- init.el
+++ init.el
@@ -50,6 +50,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(cursor-type (quote bar))
+ '(delete-selection-mode t)
+ '(display-line-numbers-grow-only nil)
+ '(global-display-line-numbers-mode t)
  '(haskell-mode-hook (quote (turn-on-haskell-indentation turn-on-haskell-doc)))
  '(package-selected-packages
    (quote
@@ -59,4 +65,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 310 :width normal :foundry "nil" :family "Monaco"))))
+ '(bold ((t (:foreground "black" :weight normal)))))
