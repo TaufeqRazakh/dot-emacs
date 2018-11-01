fedaf668518221870c836c09657ba7191c8fc0f5
Taufeq Mohammed <taufeq_star@Taufeqs-iMac.local>
2018-11-01 20:31:34 +0530

    Cleaned files and added support for scala

--- elpa/bind-key-20180513.430/bind-key-pkg.el
+++ elpa/bind-key-20180513.430/bind-key-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "bind-key" "20180513.430" "A simple way to manage personal keybindings" 'nil :commit "2b89ca4b9102baaf3f84f3fc8177c8a17288e291" :keywords '("keys" "keybinding" "config" "dotemacs") :authors '(("John Wiegley" . "johnw@newartisans.com")) :maintainer '("John Wiegley" . "johnw@newartisans.com") :url "https://github.com/jwiegley/use-package")
+(define-package "bind-key" "20180513.430" "A simple way to manage personal keybindings" 'nil :commit "23c5c431f2d238f2c0fb2509687bb94d6c375889" :keywords '("keys" "keybinding" "config" "dotemacs") :authors '(("John Wiegley" . "johnw@newartisans.com")) :maintainer '("John Wiegley" . "johnw@newartisans.com") :url "https://github.com/jwiegley/use-package")
--- init.el
+++ init.el
@@ -1,69 +1,62 @@
-
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
+;; Initialise the package system first of all.
 (package-initialize)
 
-(setenv "PATH" (concat "/usr/local/bin/:" (getenv "PATH")))
-(setq exec-path (append exec-path '("/usr/local/bin/")))
+;; Skip the default splash screen.
+(setq inhibit-startup-message t)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; Skip the default end of buffer ring.
+(setq ring-bell-function 'ignore)
 
-(use-package which-key
-  :ensure t
-  :init
-  (which-key-mode))
+;; Ensure uniform scrolling in open files.
+(setq scroll-conservatively 100)
 
-(use-package haskell-mode
-  :ensure t
-  :init
-  (haskell-mode))
+;; Figure out the current hostname.
+(setq hostname (replace-regexp-in-string "\\(^[[:space:]\n]*\\|[[:space:]\n]*$\\)" "" (with-output-to-string (call-process "hostname" nil standard-output))))
 
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-    ))
+;; Prevent tool bar and scross from appearing
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
+;;accept y/n in place of explicit yes and no
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Figure out the path to our .emacs.d by getting the path part of the
+;; current file (`init.el`).
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) (file-chase-links load-file-name))))
+
+;; load all .el files from modules dir
+(add-to-list 'load-path (concat dotfiles-dir "modules"))
+
+
+
+(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
+(setq backup-directory-alist `(("." . "~/.saves")))
+
+; (load-theme 'material t)
+(add-to-list 'default-frame-alist '(height . 50))
+(add-to-list 'default-frame-alist '(width . 160))
+(setq initial-frame-alist '((left . 100) (top . 80)))
+
+
+(require 'j-package)
+(require 'j-haskell)
+(require 'j-purescript)
+(require 'j-scala)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (nord)))
- '(custom-safe-themes
-   (quote
-    ("40da996f3246a3e99a2dff2c6b78e65307382f23db161b8316a5440b037eb72c" default)))
- '(haskell-mode-hook nil)
- '(haskell-stylish-on-save t)
+ '(haskell-mode-hook (quote (turn-on-haskell-indentation turn-on-haskell-doc)))
  '(package-selected-packages
    (quote
-    (popup\.el auto-complete nord-theme which-key use-package haskell-mode))))
-
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(setq inhibit-startup-message t)
-(setq ring-bell-function 'ignore)
-(setq scroll-conservatively 100)
-(global-linum-mode t)
-(delete-selection-mode 1)
-(setq-default indent-tabs-mode nil)
-(setq-default cursor-type 'bar)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
+    (ensime purescript-mode flycheck-haskell haskell-mode use-package paradox))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-'(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 300 :width normal :foundry "nil" :family "Menlo")))))
+ )
