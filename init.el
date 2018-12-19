7e538e6312baa2459e2f127f887b4bc9ab390d0f
Taufeq Mohammed <taufeq_star@Taufeqs-iMac.local>
2018-12-19 22:25:39 +0530

    Made a separate preference file to import into init.el

--- init.el
+++ init.el
@@ -40,7 +40,7 @@
 (add-to-list 'default-frame-alist '(width . 60))
 ; (setq initial-frame-alist '((left . 100) (top . 80)))
 
-
+(require 'j-preferences)
 (require 'j-package)
 (require 'j-haskell)
 (require 'j-purescript)
@@ -66,4 +66,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 200 :width normal :foundry "nil" :family "Monaco")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 220 :width normal :foundry "nil" :family "Monaco")))))
---
+++ modules/j-preferences.el
@@ -0,0 +1,4 @@
+(global-set-key (kbd "s-<right>") 'move-end-of-line)
+(global-set-key (kbd "s-<left>") 'move-beginning-of-line)
+
+(provide 'j-preferences)
