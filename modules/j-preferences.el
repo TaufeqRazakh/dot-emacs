2fc03f41025036e31181d4016cb7a5aae86e4ad1
Taufeq  Razakh <>
2018-12-21 00:37:30 +0530

    Comment command set to cmd - /

--- modules/j-preferences.el
+++ modules/j-preferences.el
@@ -1,4 +1,7 @@
+;; Use cmd + direction keys to move to line extremes
 (global-set-key (kbd "s-<right>") 'move-end-of-line)
 (global-set-key (kbd "s-<left>") 'move-beginning-of-line)
+;; Use cmd + / to comment and uncomment lines
+(global-set-key (kbd "s-/") 'comment-line)
 
 (provide 'j-preferences)
